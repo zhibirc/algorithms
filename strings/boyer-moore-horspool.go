6fabca95a27eee11cbc650a6b87f5a377be4bd90
Yaroslav Surilov <yaroslav.surilov@capgemini.com>
2023-10-17 11:05:06 +0300

    algo(new): Boyer-Moore-Horspool

--- strings/boyer-moore-horspool.go
+++ strings/boyer-moore-horspool.go
@@ -1,3 +1,27 @@
 package strings
 
-func main() {}
+import "fmt"
+
+func preprocess(pattern string) []int {
+	m := len(pattern)
+	res := make([]int, m)
+	d := make(map[rune]int)
+
+	for i, char := range pattern {
+		d[char] = m - i - 1
+	}
+
+	for i, char := range pattern {
+		res[i] = d[char]
+	}
+
+	return res
+}
+
+func search(needle string, haystack string) int {
+	// implement
+}
+
+func main() {
+	fmt.Printf("Index of \"bar\" in \"zoofoobarquux\": %s", search("bar", "zoofoobarquux"))
+}
