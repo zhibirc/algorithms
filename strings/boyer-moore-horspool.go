99e21c08a71cb9c4254741c30e8ce0f08774f492
Yaroslav Surilov <yaroslav.surilov@capgemini.com>
2023-10-19 11:48:52 +0300

    algo(upd): boyer-moore-horspool

--- strings/boyer-moore-horspool.go
+++ strings/boyer-moore-horspool.go
@@ -2,24 +2,54 @@ package strings
 
 import "fmt"
 
+// Construct that is called "Bad Match Table".
+// Example: preprocess("abeccde") -> [6, 5, 4, 2, 2, 1, 4]
+// Time complexity: O(2 * (M - 1)) -> O(M), i.e. linear, where M is pattern length
 func preprocess(pattern string) []int {
 	m := len(pattern)
 	res := make([]int, m)
 	d := make(map[rune]int)
 
-	for i, char := range pattern {
+	for i, char := range pattern[0 : m-1] {
 		d[char] = m - i - 1
 	}
 
-	for i, char := range pattern {
+	for i, char := range pattern[0 : m-1] {
 		res[i] = d[char]
+	}
+
+	if v, ok := d[rune(pattern[m-1])]; ok {
+		res[m-1] = v
+	} else {
+		res[m-1] = m
 	}
 
 	return res
 }
 
 func search(needle string, haystack string) int {
-	// implement
+	m := len(needle)
+	n := len(haystack)
+	d := preprocess(needle)
+
+	for i := 0; i <= n-m; i++ {
+		for j := m - 1; j >= 0; j-- {
+			if haystack[i+j] == needle[j] {
+				if j == 0 {
+					return j
+				}
+			} else {
+				if j < m-1 {
+					i += d[m-1] - 1
+				} else {
+					i += d[j] - 1
+				}
+				break
+			}
+		}
+	}
+
+	return -1
 }
 
 func main() {
