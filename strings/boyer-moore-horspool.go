b3ae00d11f84d28ef93048bc2e8be46bfe9fc803
Yaroslav Surilov <yaroslav.surilov@capgemini.com>
2023-10-21 15:28:53 +0300

    algo(upd): boyer-moore-horspool
    Add full implementation.

--- strings/boyer-moore-horspool.go
+++ strings/boyer-moore-horspool.go
@@ -1,30 +1,32 @@
+// Additional information:
+// Wikipedia: https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore%E2%80%93Horspool_algorithm
+
 package strings
 
 import "fmt"
 
-// Construct that is called "Bad Match Table".
-// Example: preprocess("abeccde") -> [6, 5, 4, 2, 2, 1, 4]
-// Time complexity: O(2 * (M - 1)) -> O(M), i.e. linear, where M is pattern length
-func preprocess(pattern string) []int {
+// Construct that is called "Bad Match Table" or "Shift Table".
+// Example: preprocess("abeccde") -> {a: 6, b: 5, e: 4, c: 2, d: 1}
+// Time complexity: O(M), i.e. linear, where M is pattern length.
+// Space complexity: O(M) in the worst case (when each symbol is unique).
+func preprocess(pattern string) map[rune]int {
 	m := len(pattern)
-	res := make([]int, m)
-	d := make(map[rune]int)
+	res := make(map[rune]int)
 
 	for i, char := range pattern[0 : m-1] {
-		d[char] = m - i - 1
+		res[char] = m - i - 1
 	}
 
-	for i, char := range pattern[0 : m-1] {
-		res[i] = d[char]
-	}
-
-	if v, ok := d[rune(pattern[m-1])]; ok {
-		res[m-1] = v
-	} else {
-		res[m-1] = m
+	if _, ok := res[rune(pattern[m-1])]; !ok {
+		res[rune(pattern[m-1])] = m
 	}
 
 	return res
+}
+
+// There is another variant of preprocessing where we construct the table for each character in presumable alphabet.
+func preprocess(pattern string) []int {
+	// implement for 256 ACSII characters table
 }
 
 func search(needle string, haystack string) int {
@@ -36,15 +38,23 @@ func search(needle string, haystack string) int {
 		for j := m - 1; j >= 0; j-- {
 			if haystack[i+j] == needle[j] {
 				if j == 0 {
-					return j
+					// success, match found at index "i"
+					return i
 				}
 			} else {
-				if j < m-1 {
-					i += d[m-1] - 1
+				if j == m-1 {
+					if v, ok := d[rune(haystack[i+j])]; ok {
+						i += v - 1
+					} else {
+						i += m - 1
+					}
 				} else {
-					i += d[j] - 1
+					if v, _ := d[rune(haystack[i+m-1])]; v < m {
+						i += v - 1
+					} else {
+						i += m - 1
+					}
 				}
-				break
 			}
 		}
 	}
@@ -53,5 +63,6 @@ func search(needle string, haystack string) int {
 }
 
 func main() {
-	fmt.Printf("Index of \"bar\" in \"zoofoobarquux\": %s", search("bar", "zoofoobarquux"))
+	// find at index 6
+	fmt.Printf("Index of \"bar\" in \"zoofoobarquux\": %d", search("bar", "zoofoobarquux"))
 }
