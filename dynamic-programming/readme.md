ea470edf91dd7acf98553c6913c13d00b9b0e6f0
Yaroslav Surilov <yaroslav.surilov@capgemini.com>
2023-10-25 12:02:48 +0300

    algo(upd): boyer-moore-horspool
    Update readme for DP.

--- dynamic-programming/readme.md
+++ dynamic-programming/readme.md
@@ -1,7 +1,10 @@
 # Basics
 
-__Dynamic Programming__ (DP) is a technique or method to solve some subclass of optimization and сombinatoric 
-problems. Ability to solve the particular problem using DP based on the _optimal substructure_ property, i.e. its optimal solution can be constructed efficiently from optimal solutions of its subproblems.
+__Dynamic Programming__ (DP) is an algorithmic paradigm or, more specifically, technique or method to solve some subclass of optimization and сombinatoric problems. Ability to solve the particular problem using DP based on the _optimal substructure_ property, i.e. its optimal solution can be constructed efficiently from optimal solutions of its subproblems. These subproblems are usually overlapped between each other, i.e. future "decisions" depend on earlier decisions.
+
+Generally, there are two ways to implement a DP algorithm:
+1. Bottom-up approach (also named tabulation).
+2. Top-down approach.
 
 
 ## Additional information
--- strings/search/boyer-moore-horspool/v2.go
+++ strings/search/boyer-moore-horspool/v2.go
@@ -2,12 +2,58 @@ package boyermoorehorspool
 
 import "fmt"
 
-// There is another variant of preprocessing where we construct the table for each character in presumable alphabet.
+// Construct that is called "Bad Match Table" or "Shift Table".
+// Example: preprocess("abeccde") -> [<97 preceding ASCII characters>, 6, 5, 2, 1, 4, <other ASCII characters>]
+// Time complexity: O(M), i.e. linear, where M is pattern length.
+// Space complexity: O(M) in the worst case (when each symbol is unique).
 func preprocess(pattern string) []int {
-	// implement for 256 ACSII characters table
+	m := len(pattern)
+	res := make([]int, 256)
+
+	for i := range res {
+		res[i] = m
+	}
+
+	for i, char := range pattern[0 : m-1] {
+		res[char] = m - i - 1
+	}
+
+	return res
 }
 
-func search(needle string, haystack string) int {}
+// @todo: should be reworked using preprocess from V2 version (ASCII table)
+func search(needle string, haystack string) int {
+	m := len(needle)
+	n := len(haystack)
+	d := preprocess(needle)
+
+	for i := 0; i <= n-m; i++ {
+		for j := m - 1; j >= 0; j-- {
+			if haystack[i+j] == needle[j] {
+				if j == 0 {
+					// success, match found at index "i"
+					return i
+				}
+			} else {
+				if j == m-1 {
+					if v, ok := d[rune(haystack[i+j])]; ok {
+						i += v - 1
+					} else {
+						i += m - 1
+					}
+				} else {
+					if v, _ := d[rune(haystack[i+m-1])]; v < m {
+						i += v - 1
+					} else {
+						i += m - 1
+					}
+				}
+			}
+		}
+	}
+
+	return -1
+}
 
 func main() {
 	// find at index 6
